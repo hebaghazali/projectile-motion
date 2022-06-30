4df553a692a752576353086542da04aa1e940455
Heba Ghazaly <hebbaghazaly@gmail.com>
2022-06-30 23:35:27 +0200

    Improve functionality

--- script.js
+++ script.js
@@ -10,11 +10,15 @@ const ctx = canvas.getContext('2d');
 canvas.width = canvasBox.offsetWidth;
 canvas.height = canvasBox.offsetHeight;
 
-const X0 = 30;
-const Y0 = canvas.height - 50;
+const groundHeight = 50;
+
+let X0 = 30;
+let Y0 = canvas.height - groundHeight;
 let X = X0;
 let Y = Y0;
 let T = 0;
+let deltaX = 0;
+let deltaY = 0;
 let velocity = Number(velocityInput.value) || 0;
 let angle = Number(angleInput.value) || 0;
 const radius = 15;
@@ -23,6 +27,9 @@ const g = 9.8;
 let v0X = velocity * Math.cos((angle * Math.PI) / 180);
 let v0Y = velocity * Math.sin((angle * Math.PI) / 180);
 
+// draw the object on load
+draw();
+
 function init() {
   window.requestAnimationFrame(animationLoop);
 }
@@ -30,22 +37,25 @@ function init() {
 function animationLoop() {
   update();
   draw();
-  const yIsNotOnEdge = Y < canvas.height - (radius + 35);
-  const xIsNotOnEdge = X < canvas.width - (radius + 2);
 
-  if (yIsNotOnEdge && xIsNotOnEdge) window.requestAnimationFrame(animationLoop);
+  const xIsOnEdge = X >= canvas.width - radius || X <= radius;
+
+  if (objectIsMoving(X, Y)) window.requestAnimationFrame(animationLoop);
   else {
-    window.cancelAnimationFrame(animationLoop);
-
-    if (Math.round(X) !== X0) launchBtn.setAttribute('disabled', true);
+    if (xIsOnEdge) launchBtn.setAttribute('disabled', true);
+    else {
+      // Move the object to its initial Y value if it doesn't touch the final X value
+      Y += deltaY;
+      draw();
+    }
   }
 }
 
 function update() {
-  T += 0.15;
+  T += 0.16;
 
-  const deltaX = v0X * T;
-  const deltaY = v0Y * T - 0.5 * g * Math.pow(T, 2);
+  deltaX = v0X * T;
+  deltaY = v0Y * T - 0.5 * g * Math.pow(T, 2);
   X = X0 + deltaX;
   Y = Y0 - deltaY;
 }
@@ -59,41 +69,56 @@ function draw() {
 }
 
 function reset(e) {
-  const yIsNotOnEdge = Y < canvas.height - (radius + 35);
-  const xIsNotOnEdge = X < canvas.width - (radius + 2);
-  if (yIsNotOnEdge && xIsNotOnEdge) return;
+  if (objectIsMoving(X, Y)) return;
 
-  X = 30;
-  Y = canvas.height - 50;
+  if (e && e.target.id === 'reset') {
+    launchBtn.removeAttribute('disabled');
+    X0 = 30;
+  } else {
+    X0 = X;
+  }
+
+  X = X0;
+  Y = canvas.height - groundHeight;
   T = 0;
 
   v0X = velocity * Math.cos((angle * Math.PI) / 180);
   v0Y = velocity * Math.sin((angle * Math.PI) / 180);
 
   draw();
-
-  if (e && e.target.id === 'reset') {
-    launchBtn.removeAttribute('disabled');
-  }
 }
 
 function launch() {
-  const yIsNotOnEdge = Y < canvas.height - (radius + 35);
-  const xIsNotOnEdge = X < canvas.width - (radius + 2);
-  if (yIsNotOnEdge && xIsNotOnEdge) return;
+  console.log(Y);
 
-  if (Math.round(X) !== X0) {
-    return;
-  }
+  if (objectIsMoving(X, Y)) return;
 
   velocity = Number(velocityInput.value) || 0;
   angle = Number(angleInput.value) || 0;
-
   reset();
   init();
 }
 
-draw();
-
 launchBtn.addEventListener('click', launch);
 resetBtn.addEventListener('click', reset);
+
+function objectIsMoving(x, y) {
+  const yIsOnEdge = y >= canvas.height - groundHeight;
+  const xIsOnEdge = x >= canvas.width - radius || X <= radius;
+
+  return !yIsOnEdge && !xIsOnEdge;
+}
+
+/////////////////////////////////////////////////////////
+
+// Input validation
+
+function validateInput(input, maxNum) {
+  return function () {
+    if (input.value < 0) input.value = 0;
+    if (input.value > maxNum) input.value = maxNum;
+  };
+}
+
+velocityInput.addEventListener('change', validateInput(velocityInput, 100));
+angleInput.addEventListener('change', validateInput(angleInput, 180));
